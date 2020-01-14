b12aa3745a7eddca3af531b6cdc3a40dedb7911f
ndasilver <ndasilver@gmail.com>
2020-01-14 03:24:56 +0200

    created router file

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello this is the home page");
+});
+
+app.listen(3000, () => {
+    console.log("app is listening to port 3000");
+});
